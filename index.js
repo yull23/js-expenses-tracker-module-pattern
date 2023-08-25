250bf853e962dc1474a95b1a051e01257ed664dc
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-08-24 19:18:13 -0500

    Adding expense view without 'delete' anchor

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+// Store
+const idGenerator = (function () {
+  let id = 0;
+  return {
+    next() {
+      return id++;
+    },
+  };
+})();
+
+// Se crea Store
+const Store = (function () {
+  const initialExpenses = [
+    {
+      id: idGenerator.next(),
+      category: "shopping",
+      description: "nintendo",
+      amount: "500",
+    },
+    {
+      id: idGenerator.next(),
+      category: "shopping",
+      description: "tequila",
+      amount: "100",
+    },
+  ];
+
+  // Retorna este objeto y nos olvidamos de Initial Expenses
+  return {
+    expenses:
+      JSON.parse(localStorage.getItem("ExpenseList")) || initialExpenses,
+    addExpense(newExpense) {
+      this.expenses.push(newExpense);
+      localStorage.setItem("ExpenseList", JSON.stringify(this.expenses));
+    },
+    removeExpense(expense) {
+      const indexRemove = this.expenses.findIndex(
+        (item) => item.id === parseInt(expense.id)
+      );
+      this.expenses.splices(indexRemove, 1);
+      localStorage.setItem("ExpenseList", JSON.stringify(this.expenses));
+    },
+  };
+})();
+
+// Se crea el load para cargar modulos al DOM
 const DOMHandler = function (parentSelector) {
   const parent = document.querySelector(parentSelector);
   if (!parent) throw new Error("Parent not found");
@@ -13,26 +59,26 @@ const DOMHandler = function (parentSelector) {
     },
   };
 };
-
 const App = DOMHandler("#root");
 
-// Create the Header module, which returns an object with two elements
+// Create the module, which returns an object with two elements
 // The first element is toString() = returns the html structure in a string
 // The second element is addListeners() = when executed it creates events related to the module
 
-const createHeader = function () {
+// Header
+const Header = (function () {
   const template = `
-    <header class="header flex">
-      <div class="nav flex">
+    <header class="header f">
+      <div class="nav f">
         <h1 class="header__title upc">
           <a href="index.html" class="pl-100">expenses tracker</a>
         </h1>
         <nav class="navbar js-navbar">
           <ul class="navbar__list">
-            <li class="navbar__item flex-cc">
+            <li class="navbar__item f-cc">
               <a class="navbar__link" href="index.html">list expenses</a>
             </li>
-            <li class="navbar__item flex-cc">
+            <li class="navbar__item f-cc">
               <a class="navbar__link" href="#">add expenses</a>
             </li>
           </ul>
@@ -50,14 +96,17 @@ const createHeader = function () {
           />
         </div>
       </div>
-    </header>`;
+    </header>
+    `;
   const handlerClick = function () {
     const navMenu = document.querySelector(".js-navmenu");
     const navBar = document.querySelector(".js-navbar");
+    const main = document.querySelector(".js-main");
 
     navMenu.addEventListener("click", (event) => {
       navMenu.classList.toggle("navmenu-open");
       navBar.classList.toggle("navmenu-open");
+      main.classList.toggle("section");
     });
   };
   return {
@@ -68,6 +117,54 @@ const createHeader = function () {
       handlerClick();
     },
   };
-};
-const header = createHeader();
-App.load(header);
+})();
+
+const Main = (function () {
+  const expenseFormat = function (expense) {
+    return `
+    <div class="expense f br-050 plr-150 ptb-075">
+      <div class="expense__description f fd-c w-100">
+        <span class="expense__type fw-bold">${expense.category}</span>
+        <span class="expense__product">${expense.description}</span>
+      </div>
+      <span class="expense__price f-cc">$${expense.amount}</span>
+      <a href="#" class="expense__action f-cc fw-bold" data-id=${expense.id}>delete</a>
+    </div>
+    `;
+  };
+
+  const template = `
+    <main class="main container-md mt-075 js-main">
+      <section class="expenselist f fd-c">
+        ${Store.expenses.map((element) => expenseFormat(element)).join("")}
+        <a href="#" class="expenses-new__link br-030 js-add-new-expense"
+          >add new expense</a
+        >
+      </section>
+    </main>
+    `;
+
+  return {
+    toString() {
+      return template;
+    },
+  };
+})();
+
+const layaout = (function () {
+  const template = `
+  ${Header}
+  ${Main}
+  `;
+  return {
+    toString() {
+      return template;
+    },
+    addListeners() {
+      Header.addListeners();
+      // Main.addListeners();
+    },
+  };
+})();
+
+App.load(layaout);
